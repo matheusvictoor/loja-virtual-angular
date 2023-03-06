37212366c87534e42d9d898724cec5a695e5f0b3
Matheus Victor <matheusvictor.dev@gmail.com>
2023-03-06 19:44:24 -0300

    diciona comentários ao código

--- src/app/barra-pesquisa/barra-pesquisa.component.ts
+++ src/app/barra-pesquisa/barra-pesquisa.component.ts
@@ -7,12 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./barra-pesquisa.component.css']
 })
 export class BarraPesquisaComponent {
+  //  armazena o valor da barra de pesquisa
   descricao = "";
 
   constructor(
+    // método é usado para injetar dependências na classe
     private router: Router
   ) {}
 
+  // verifica se a barra de pesquisa possui algum valor. Se houver um valor, ele navegará para a página produtos e adicionará o valor da barra de pesquisa como um parâmetro de consulta na URL
   pesquisar() {
     if(this.descricao) {
       this.router.navigate(["produtos"], { queryParams: { descricao: this.descricao }})
